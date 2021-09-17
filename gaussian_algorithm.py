bfb328c71b4b94892339f30024ce059c3a695045
Azen0n <jack.2013.ghost@gmail.com>
2021-09-17 14:43:05 +0500

    Add back substitution

--- gaussian_algorithm.py
+++ gaussian_algorithm.py
@@ -7,12 +7,12 @@ def forward(a, b):
     # Размерность матрицы
     n = len(a[0])
 
-    for k in range(0, n):
-        for i in range(k + 1, n):
+    for k in range(0, n):                           # Проходим каждый столбец, одна итерация этого цикла зануляет его
+        for i in range(k + 1, n):                   # Элемент под главной диагональю
             r = a[i][k] / a[k][k]
-            for j in range(k, n):
+            for j in range(k, n):                   # Вычитаем из всей строки другую, k-й элемент равен нулю
                 a[i][j] = a[i][j] - r * a[k][j]
-            b[i] = b[i] - b[k] * r
+            b[i] = b[i] - b[k] * r                  # То же самое со столбцом свободных членов
 
     return a, b
 
@@ -22,9 +22,26 @@ def backward(a, b):
     # Размерность матрицы
     n = len(a[0])
 
+    # Вектор со значениями x
+    x = np.zeros(n)
+    x[n - 1] = b[n - 1] / a[n - 1][n - 1]   # Вычисление x_n
+
+    for i in range(n - 2, -1, -1):          # Вычисление остальных x, начиная с предпоследнего
+        sum = 0
+        for j in range(i + 1, n):           # В цикле умножаем уже вычисленные значения x на значения матрицы a
+            sum += a[i][j] * x[j]
+        x[i] = (b[i] - sum) / a[i][i]       # И делим на соответсвующее значение матрицы a на главной диагонали
+
+    return x
+
 
 if __name__ == '__main__':
-    print('Матрица коэффициентов:\n', a_test)
-    matrix = gaussian_forward(a_test, b_test)
+    print('Исходная матрица коэффициентов:\n', a)
+    print('Исходный столбец свободных членов:\n', b)
+    matrix = forward(a, b)
     print('\nМатрица коэффициентов после прямого хода:\n', matrix[0])
     print('Столбец свободных членов:\n', matrix[1])
+
+    answers = backward(matrix[0], matrix[1])
+    print('\nВектор со значениями x:\n', answers)
+
