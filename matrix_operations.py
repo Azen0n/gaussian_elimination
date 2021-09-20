9d5f2352c559d63f1a0aaf61d747e29ac2987fe9
Azen0n <jack.2013.ghost@gmail.com>
2021-09-20 21:42:50 +0500

    Add identity and inverse functions

--- matrix_operations.py
+++ matrix_operations.py
@@ -1,5 +1,6 @@
 import numpy as np
 from gaussian_algorithm import forward
+from data import a_test2
 
 
 def determinant(a):
@@ -12,3 +13,34 @@ def determinant(a):
     # Некрасиво, зато не требует еще один аргумент и исходную функцию можно не трогать
     triangular, b = forward(a, np.zeros((len(a[0]), 1)))
     return np.trace(triangular)
+
+
+def identity(n):
+    """
+    Функция создает единичную матрицу размера n.
+
+    :param n: размерность матрицы
+    :return: единичная матрица
+    """
+    a = np.zeros((n, n))
+
+    for i in range(n):
+        a[i][i] = 1
+
+    return a
+
+
+def inverse(a):
+    """
+    Функция вычисляет обратную матрицу методом Гаусса.
+
+    :param a: исходная матрица
+    :return: обратная матрица a^-1
+    """
+    # Размерность матрицы
+    n = len(a[0])
+
+    identity_matrix = identity(n)
+    a, b = forward(a, identity_matrix, method='identity')
+
+    return b
