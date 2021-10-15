3b9e23b09c9fb0c419621537090ea7923f731ec2
Azen0n <jack.2013.ghost@gmail.com>
2021-10-15 18:16:34 +0500

    Add iterative root-finding method, print results

--- nonlinear_equations.py
+++ nonlinear_equations.py
@@ -1,26 +1,108 @@
 import numpy as np
 import root_finding_methods as root
-from data import function, derivative
+import data
 from prettytable import PrettyTable
+from matplotlib import pyplot as plt
+
+
+# График function
+x = np.array(np.arange(-0.99, 0.99, 0.05))
+y = data.function(x)
+
+plt.plot(x, y)
+y1 = np.zeros(x.shape)
+plt.plot(x, y1, c='red')
+
+plt.plot(0.4, 0, 'ro', ms=4)
+plt.plot([0.4, 0.4], [0, -5.59], '--', c='gray')
+
+plt.text(0.15, 2.0, r'$f(x)=ln\frac{1 + x}{1 - x}-cos^2(x)$')
+plt.text(-0.49, 0.25, r'$f(x)=0$')
+plt.text(0.3, 0.25, r'$ξ$')
+
+plt.title('Графическое отделение корня уравнения')
+plt.xlabel('x')
+plt.ylabel('f(x)')
+plt.grid()
+plt.show()
 
 
 k = 30
-eps = 0.00001
-answer1, k1 = root.bisection(-0.4, 0.75, k, eps, function)
-print(answer1)
+eps = 1e-06
+a = 0.25        # Интервал [a; b]
+b = 0.5
+root_bisection, k_bisection, plot_data_bisection = root.bisection(a, b, k, eps, data.function)
+root_chord, k_chord, plot_data_chord = root.chord(a, b, k, eps, data.function)
+root_newton, k_newton, plot_data_newton = root.newton(a, k, eps, data.function, data.derivative)
+root_secant, k_secant, plot_data_secant = root.secant(a, a + 0.1, k, eps, data.function)
 
-answer2, k2 = root.chord(-0.4, 0.75, k, eps, function)
-print(answer2)
 
-answer3, k3 = root.newton(-0.4, k, eps, function, derivative)
-print(answer3)
-
-answer4, k4 = root.secant(0.1, 0.9, k, eps, function)
-print(answer4)
-
-table = PrettyTable(['Метод', 'Начальное приближение', 'Полученный корень', 'Число итераций', 'Величина погрешности ε', 'Проверка f(ξ)'])
-table.add_row(['Метод бисекций',    [-0.4, 0.75],   answer1, k1, eps, np.round(function(answer1), 2) == 0.0])
-table.add_row(['Метод хорд',        [-0.4, 0.75],   answer2, k2, eps, np.round(function(answer2), 2) == 0.0])
-table.add_row(['Метод Ньютона',     -0.4,           answer3, k3, eps, np.round(function(answer3), 2) == 0.0])
-table.add_row(['Метод секущих',     0.9,            answer4, k4, eps, np.round(function(answer4), 2) == 0.0])
+table = PrettyTable(['Метод', 'Начальное приближение', 'Полученный корень',
+                     'Число итераций', 'Величина погрешности ε', 'Проверка f(ξ)'])
+table.add_row(['Метод бисекций', plot_data_bisection[0], root_bisection, k_bisection, eps, np.round(data.function(root_bisection), 2) == 0.0])
+table.add_row(['Метод хорд', plot_data_chord[0], root_chord, k_chord, eps, np.round(data.function(root_chord), 2) == 0.0])
+table.add_row(['Метод Ньютона', plot_data_newton[0], root_newton, k_newton, eps, np.round(data.function(root_newton), 2) == 0.0])
+table.add_row(['Метод секущих', plot_data_secant[0], root_secant, k_secant, eps, np.round(data.function(root_secant), 2) == 0.0])
 print(table)
+
+
+def plot(plot_data, k, title):
+    plt.plot(data.function(plot_data))
+    plt.title(title)
+    plt.xlabel('Количество итераций')
+    plt.ylabel('f(x)')
+    plt.xticks(range(0, k + 1))
+    plt.grid(axis='y')
+    plt.show()
+
+
+# Графики изменения f(x) четырех методов
+plot(plot_data_bisection, k_bisection, 'Метод бисекций')
+plot(plot_data_chord, k_chord, 'Метод хорд')
+plot(plot_data_newton, k_newton, 'Метод Ньютона')
+plot(plot_data_secant, k_secant, 'Метод секущих')
+
+
+# Метод простых итераций: таблица с результатами для разных точностей и график изменения f(x)
+def print_iterative_results(function, lambda_value=None):
+    table = PrettyTable(['Начальное приближение', 'Полученный корень',
+                                   'Число итераций', 'Величина погрешности ε', 'Проверка f(ξ)'])
+    for eps_iterative in [1e-03, 1e-05, 1e-08]:
+        if lambda_value is not None:
+            root_iterative, k_iterative, plot_data_iterative = root.iterative(a, k, eps_iterative, function, lambda_value)
+            table.add_row([plot_data_iterative[0], root_iterative, k_iterative, eps_iterative,
+                                     np.round(data.function(root_iterative), 2) == 0.0])
+        else:
+            root_iterative, k_iterative, plot_data_iterative = root.iterative(a, k, eps_iterative, function)
+            table.add_row([plot_data_iterative[0], root_iterative, k_iterative, eps_iterative,
+                                     np.round(function(root_iterative), 2) == 0.0])
+    print(table)
+    plot(plot_data_iterative, k_iterative, 'Метод простых итераций')
+
+
+print('\nМетод простых итераций')
+derivative_value = data.iterative_derivative(root_bisection)
+print(f'Проверка условия сходимости: |f\'(ξ)| = {np.absolute(derivative_value):.2f} '
+      f'{"<= q < 1" if derivative_value < 1 else "> 1 — условие сходимости не выполняется"}')
+print_iterative_results(data.function)
+
+
+# Расчеты для двух наборов значений λ и q
+alpha_iterative = -0.15
+gamma_iterative = -0.35
+
+lambda_monotone = 1.0 / gamma_iterative
+q_monotone = 1.0 - alpha_iterative / gamma_iterative
+
+print(f'\nМетод простых итераций при λ = {lambda_monotone} и q = {q_monotone} (монотонная сходимость)')
+print(f'Проверка условия сходимости: |f\'(ξ)| = {np.absolute(derivative_value):.2f} '
+      f'{"<= q < 1" if np.absolute(derivative_value) <= q_monotone else ">= q — условие сходимости не выполняется"}')
+print_iterative_results(data.iterative_function_lambda, lambda_monotone)
+
+lambda_optimal = 2.0 / (alpha_iterative + gamma_iterative)
+q_optimal = (gamma_iterative - alpha_iterative) / (gamma_iterative + alpha_iterative)
+
+print(f'\nМетод простых итераций при λ = {lambda_optimal} и q = {q_optimal} (оптимальная сходимость)')
+print(f'Проверка условия сходимости: |f\'(ξ)| = {np.absolute(derivative_value):.2f} '
+      f'{"<= q < 1" if np.absolute(derivative_value) <= q_optimal else ">= q — условие сходимости не выполняется"}')
+print_iterative_results(data.iterative_function_lambda, lambda_optimal)
--- root_finding_methods.py
+++ root_finding_methods.py
@@ -1,4 +1,5 @@
 import numpy as np
+from data import iterative_function
 
 
 def bisection(start, end, k, eps, function):
@@ -8,28 +9,31 @@ def bisection(start, end, k, eps, function):
     :param start: начальное значение интервала a
     :param end: конечное значение интервала b
     :param k: количество итераций
-    :param eps: значение погрешности
+    :param eps: значение точности
     :param function: непрерывная функция f(x)
-    :return: корень уравнения и количество пройденных итераций
+    :return: корень уравнения, количество пройденных итераций и список со всеми найденными значениями x
     """
     mid_prev = 0.0
+    plot_data = []
     for current_k in range(k):
         mid = (start + end) / 2.0
 
         if np.absolute(function(mid) - function(mid_prev)) < eps:
-            return mid, current_k
+            return mid, current_k, plot_data
         mid_prev = mid
+
+        plot_data.append(mid)
 
         if function(mid) * function(start) < 0:
             end = mid
         elif function(mid) * function(end) < 0:
             start = mid
         elif function(mid) == 0:
-            return mid, current_k
+            return mid, current_k, plot_data
         else:
             raise Exception('Bisection method: bad initial interval.')
 
-    return (start + end) / 2.0, k
+    return (start + end) / 2.0, k, plot_data
 
 
 def chord(start, end, k, eps, function):
@@ -39,16 +43,18 @@ def chord(start, end, k, eps, function):
     :param start: начальное значение интервала a
     :param end: конечное значение интервала b
     :param k: количество итераций
-    :param eps: значение погрешности
+    :param eps: значение точности
     :param function: непрерывная функция f(x)
-    :return: корень уравнения и количество пройденных итераций
+    :return: корень уравнения, количество пройденных итераций и список со всеми найденными значениями x
     """
     x_prev = 0.0
+    plot_data = []
     for current_k in range(k):
         x = start - function(start) * (end - start) / (function(end) - function(start))
+        plot_data.append(x)
 
         if np.absolute(function(x) - function(x_prev)) < eps:
-            return x, current_k
+            return x, current_k, plot_data
         x_prev = x
 
         if function(x) * function(start) < 0:
@@ -56,55 +62,87 @@ def chord(start, end, k, eps, function):
         elif function(x) * function(end) < 0:
             start = x
         elif function(x) == 0 or np.absolute(function(x)) < eps:
-            return x, current_k
+            return x, current_k, plot_data
         else:
             raise Exception('Secant method: bad initial interval.')
 
-    return start - function(start) * (end - start) / (function(end) - function(start)), k
+    return start - function(start) * (end - start) / (function(end) - function(start)), k, plot_data
 
 
 def newton(x, k, eps, function, derivative):
     """
-    Метод Ньютона для нахождения корня функции f(x) на отрезке [a, b].
+    Метод Ньютона для нахождения корня функции f(x).
 
-    :param x: начальное значение x
+    :param x: начальное приближение x
     :param k: количество итераций
-    :param eps: значение погрешности
+    :param eps: значение точности
     :param function: дифференцируемая функция f(x)
     :param derivative: производная функции f'(x)
-    :return: корень уравнения и количество пройденных итераций
+    :return: корень уравнения, количество пройденных итераций и список со всеми найденными значениями x
     """
     x_prev = 0.0
+    plot_data = []
     for current_k in range(k):
+        plot_data.append(x)
+
         x = x - function(x) / derivative(x)
 
         if np.absolute(function(x) - function(x_prev)) < eps:
-            return x, current_k
+            return x, current_k, plot_data
         x_prev = x
 
         if derivative(x) == 0:
             raise Exception('Newton method: zero derivative.')
 
-    return x, k
+    return x, k, plot_data
 
 
-# Doesn't work
 def secant(x_prev, x, k, eps, function):
     """
-    Метод секущих для нахождения корня функции f(x) на отрезке [a, b].
+    Метод секущих для нахождения корня функции f(x).
 
-    :param x: начальное значение x
+    :param x_prev: первое начальное приближение x
+    :param x: второе начальное приближение x
     :param k: количество итераций
-    :param eps: значение погрешности
+    :param eps: значение точности
     :param function: дифференцируемая функция f(x)
-    :return: корень уравнения и количество пройденных итераций
+    :return: корень уравнения, количество пройденных итераций и список со всеми найденными значениями x
     """
+    plot_data = [x_prev]
     for current_k in range(k):
+        plot_data.append(x)
 
         if np.absolute(function(x) - function(x_prev)) < eps:
-            return x, current_k
+            return x, current_k, plot_data
 
-        x = (x_prev * function(x) - x * function(x_prev)) / (function(x) - function(x_prev))
+        x, x_prev = x - function(x) * (x - x_prev) / (function(x) - function(x_prev)), x
+
+    return x, k, plot_data
+
+
+def iterative(x, k, eps, function, lambda_value=None):
+    """
+    Метод простых итераций для нахождения корня функции f(x).
+
+    :param x: начальное приближение x
+    :param k: количество итераций
+    :param eps: значение точности
+    :param function: дифференцируемая функция f(x)
+    :param lambda_value: значение λ (по умолчанию None)
+    :return: корень уравнения, количество пройденных итераций и список со всеми найденными значениями x
+    """
+    plot_data = []
+    for current_k in range(k):
+        plot_data.append(x)
+
         x_prev = x
+        x = iterative_function(x)
 
-    return x, k
+        if lambda_value is not None:
+            if np.absolute(function(x, lambda_value) - function(x_prev, lambda_value)) < eps:
+                return x, current_k, plot_data
+        else:
+            if np.absolute(function(x) - function(x_prev)) < eps:
+                return x, current_k, plot_data
+
+    return x, k, plot_data
